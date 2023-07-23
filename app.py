652ab4e151ae898849f465a4ef94f78ba92d6b61
Arnav Jain <64888928+BlurryFace04@users.noreply.github.com>
2023-07-23 12:34:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,17 @@ st.markdown("<br>", unsafe_allow_html=True)
 st.subheader("1. Upload File")
 file_path = st.file_uploader("Upload a file")
 
-if file_path is not None:
+if file_path is not None and st.button("Upload File", key="upload_file"):
     # Upload the file
     upload_response = conn.upload_file(file_path)
-    st.write(upload_response)
+    st.session_state['upload_response'] = upload_response
+
+if 'upload_response' in st.session_state:
+    st.write(st.session_state['upload_response'])
 
     # Unpin button
     if st.button("Unpin this file", key="unpin_file"):
-        ipfs_hash = upload_response['IpfsHash']
+        ipfs_hash = st.session_state['upload_response']['IpfsHash']
         unpin_response = conn.unpin(ipfs_hash)
         st.write(unpin_response)
 
@@ -35,14 +38,17 @@ if file_path is not None:
 # st.subheader("Upload Directory")
 # directory_path = st.text_input("Enter the absolute path of a directory")
 
-# if directory_path:
+# if directory_path and st.button("Upload Directory", key="upload_directory"):
 #     # Upload the directory
 #     upload_response = conn.upload_directory(directory_path)
-#     st.write(upload_response)
+#     st.session_state['directory_upload_response'] = upload_response
+
+# if 'directory_upload_response' in st.session_state:
+#     st.write(st.session_state['directory_upload_response'])
 
 #     # Unpin button
 #     if st.button("Unpin this directory", key="unpin_directory"):
-#         ipfs_hash = upload_response['IpfsHash']
+#         ipfs_hash = st.session_state['directory_upload_response']['IpfsHash']
 #         unpin_response = conn.unpin(ipfs_hash)
 #         st.write(unpin_response)
 
@@ -52,15 +58,18 @@ with st.expander("Options"):
     cid_input = st.text_input("Enter a CID to pin")
     name_input = st.text_input("Enter a name for the pin")
 
-    if cid_input and name_input:
+    if cid_input and name_input and st.button("Pin by CID", key="pin_cid"):
         cid_response = conn.pin_by_cid(cid_input, name_input)
-        st.write(cid_response)
+        st.session_state['cid_response'] = cid_response
 
-        # Unpin button
-        if st.button("Unpin this file", key="unpin_cid"):
-            ipfs_hash = cid_response['IpfsHash']
-            unpin_response = conn.unpin(ipfs_hash)
-            st.write(unpin_response)
+if 'cid_response' in st.session_state:
+    st.write(st.session_state['cid_response'])
+
+    # Unpin button
+    if st.button("Unpin this file", key="unpin_cid"):
+        ipfs_hash = st.session_state['cid_response']['IpfsHash']
+        unpin_response = conn.unpin(ipfs_hash)
+        st.write(unpin_response)
 
 # Pin JSON
 st.subheader("3. Pin JSON")
@@ -68,20 +77,23 @@ with st.expander("Options"):
     json_content_input = st.text_input("Enter JSON content to pin (as a JSON string)")
     json_name_input = st.text_input("Enter a name for the JSON pin")
 
-    if json_content_input and json_name_input:
+    if json_content_input and json_name_input and st.button("Pin JSON", key="pin_json"):
         try:
             json_content = json.loads(json_content_input)
             json_response = conn.pin_by_json(json_content, json_name_input)
-            st.write(json_response)
-
-            # Unpin button
-            if st.button("Unpin this file", key="unpin_json"):
-                ipfs_hash = json_response['IpfsHash']
-                unpin_response = conn.unpin(ipfs_hash)
-                st.write(unpin_response)
+            st.session_state['json_response'] = json_response
 
         except json.decoder.JSONDecodeError:
             st.error("Invalid JSON string")
+
+if 'json_response' in st.session_state:
+    st.write(st.session_state['json_response'])
+
+    # Unpin button
+    if st.button("Unpin this file", key="unpin_json"):
+        ipfs_hash = st.session_state['json_response']['IpfsHash']
+        unpin_response = conn.unpin(ipfs_hash)
+        st.write(unpin_response)
 
 # Update metadata
 st.subheader("4. Update Metadata")
@@ -104,30 +116,45 @@ with st.expander("Options"):
             update_metadata_response = conn.update_metadata(ipfs_hash_input, name_input, keyvalues)
         else:
             update_metadata_response = conn.update_metadata(ipfs_hash_input, name_input)
-        st.write(update_metadata_response)
+        st.session_state['update_metadata_response'] = update_metadata_response
+
+if 'update_metadata_response' in st.session_state:
+    st.write(st.session_state['update_metadata_response'])
 
 # Unpin
 st.subheader("5. Unpin")
 unpin_hash_input = st.text_input("Enter an IPFS hash to unpin")
-if unpin_hash_input:
+if unpin_hash_input and st.button("Unpin", key="unpin"):
     unpin_response = conn.unpin(unpin_hash_input)
-    st.write(unpin_response)
+    st.session_state['unpin_response'] = unpin_response
+
+if 'unpin_response' in st.session_state:
+    st.write(st.session_state['unpin_response'])
 
 # Get with a specific hash
 st.subheader("6. Get Info")
 hash_input = st.text_input("Enter an IPFS hash to get its info")
-if hash_input:
+if hash_input and st.button("Get Info", key="get_info"):
     hash_response = conn.query(hash_contains=hash_input, ttl=0)
-    st.write(hash_response)
+    st.session_state['hash_response'] = hash_response
+
+if 'hash_response' in st.session_state:
+    st.write(st.session_state['hash_response'])
 
 # Get with a specific status
 st.subheader("7. Query Data")
 status_options = [None, "all", "pinned", "unpinned"]
 status = st.selectbox("Select pin status to query the data", status_options)
 
-if status:
+if status and st.button("Query Data", key="query_data"):
     pins_response = conn.query(status=status, ttl=0)
-    st.write(pins_response)
+    st.session_state['pins_response'] = pins_response
+
+if 'pins_response' in st.session_state and status is not None:
+    st.write(st.session_state['pins_response'])
+
+if status is None:
+    st.session_state.pop('pins_response', None)
 
 # List by jobs
 st.subheader("8. List by Jobs")
@@ -135,15 +162,24 @@ jobs_options = [None, "prechecking", "searching", "retrieving", "expired", "over
                 "invalid_object", "bad_host_node"]
 jobs = st.selectbox("Select job status to list the data", jobs_options)
 
-if jobs:
+if jobs and st.button("List by Jobs", key="list_jobs"):
     jobs_response = conn.list_by_jobs(status=jobs, ttl=0)
-    st.write(jobs_response)
+    st.session_state['jobs_response'] = jobs_response
+
+if 'jobs_response' in st.session_state and jobs is not None:
+    st.write(st.session_state['jobs_response'])
+
+if jobs is None:
+    st.session_state.pop('jobs_response', None)
 
 # Get data usage
 st.subheader("9. Data Usage")
 if st.button("Get data usage", key="data_usage"):
     usage_response = conn.data_usage()
-    st.write(usage_response)
+    st.session_state['usage_response'] = usage_response
+
+if 'usage_response' in st.session_state:
+    st.write(st.session_state['usage_response'])
 
 st.markdown("---")
 st.markdown("Made with :heart: by Blurry Face")
